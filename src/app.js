6c0f1984e71e0872c6ac86230aa2dfd37bc72ce2
PAULO EDUARDO DE MELO LAREDO <paulo.laredo@redeoba.com.br>
2020-04-08 12:24:26 -0300

    Projects Repositories (GET. POST. PUT, UPDATE, DELETE)

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { uuid, isUuid } = require('uuidv4');
 const cors = require("cors");
 
 // const { uuid } = require("uuidv4");
@@ -10,24 +11,68 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (request, response) => {  
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes:0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).send();
+  }
+
+  const repository = {
+    id,
+    url, 
+    title, 
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  };
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const {id} = req.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+ 
+  if(repositoryIndex < 0) {
+    return res.status(400).send();
+  } 
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+    
+  if(!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
